7461ad1cca2974eb2675a025fcd8adfb2ffe72e7
Vamsi Nadella <vamsinadella@users.noreply.github.com>
2015-01-09 16:42:55 -0800

    Initial Commit

---
+++ OnSpotReview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnSpotReview
+//
+//  Created by Vamsikrishna Nadella on 1/9/15.
+//  Copyright (c) 2015 appsnideas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OnSpotReview/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  OnSpotReview
+//
+//  Created by Vamsikrishna Nadella on 1/9/15.
+//  Copyright (c) 2015 appsnideas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
